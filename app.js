1a614c97d3714283c275f7016f81aab04fb6e1e9
Sarah Ghandi <ghandi.sarah33@gmail.com>
2023-10-01 18:05:53 +0200

    Adding login ans signup functionality

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const mongoose = require('mongoose');
-// const Book = require('./models/Book');
 
 
 const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
 
-
-// conexion base de données
-mongoose.connect('mongodb+srv://new-user33:ghandi212@cluster0.ej433a1.mongodb.net/?retryWrites=true&w=majority',
+// connexion base de données
+mongoose.connect('mongodb+srv://new-user33:<password>@cluster0.ej433a1.mongodb.net/?retryWrites=true&w=majority',
 { useNewUrlParser: true,
   useUnifiedTopology: true })
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// entete
+// entete gestion des CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.use('/api/book' , bookRoutes);
-  
+  app.use('/api/books' , bookRoutes);
+  app.use('/api/auth', userRoutes);
+
   module.exports = app;
--- routes/book.js
+++ routes/book.js
@@ -1,63 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const Book = require('../models/Book');
+const bookCtrl = require('../controllers/book');
 
 
-// route post - enregistre un livre
-router.post('/', );
-
- 
-
-  // modification d'un livre
-  router.put('/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  // route suppression un livre
-  router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  // route get pour répondre à un endpoint précis - un seul livre
-  router.get('/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-  });
-
-   // récupération des données - tout les livres
-   router.get('/', (req, res, next) => {
-    Book.find()
-      .then(books => res.status(200).json(books))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  // récupération livres
-  router.get('/', (req, res, next) => {
-    const books = [
-      {
-        userId: 'ijididddd',
-        title: 'Il était une fois à Safi',
-        author: 'Ghandi Sarah',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        year: 2023,
-        genre: 'autobiographie, romance',
-        ratings: [
-            {
-            userId: 'eddde',
-            grade: 4,
-            }
-        ],
-        averageRating: 2,
-    },
-    ];
-    res.status(200).json(books);
-  });
-
+  router.post('/', bookCtrl.createBook);
+  router.put('/:id', bookCtrl.modifyBook);
+  router.delete('/:id', bookCtrl.deleteBook);
+  router.get('/:id', bookCtrl.getOneBook);
+  router.get('/', bookCtrl.getAllBook);
 
 module.exports = router;
